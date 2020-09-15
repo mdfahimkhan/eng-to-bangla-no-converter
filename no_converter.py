e28aa8f04b554296afc06275fb03d02f2bbf5024
mdfahimkhan <69118713+mdfahimkhan@users.noreply.github.com>
2020-09-15 22:01:03 +0600

    Update no_converter.py

--- no_converter.py
+++ no_converter.py
@@ -16,41 +16,19 @@ for a in new:
       alu= alu+hala
 print("Digit without comma: "+alu)
 string = list(alu)
+numerical_range = []
 
-
-if len(string)==7:
-      for i,j in enumerate([2,4]):
+if (len(string)%2)!=0:
+      for a in range(1, len(string)-2,2):
+             numerical_range.append(a)
+      for i,j in enumerate(numerical_range):
              string.insert(i+j, ",")
       print(''.join(string))
-elif len(string)==3 or len(string)<3:
-      print(''.join(string))
-elif len(string)==4:
-      for i,j in enumerate([1]):
-             string.insert(i+j, ",")
-      print(''.join(string))
-elif len(string)==5:
-      for i,j in enumerate([3]):
-             string.insert(i+j, ",")
-      print(''.join(string))
-elif len(string)==6:
-      for i,j in enumerate([1,3]):
-             string.insert(i+j, ",")
-      print(''.join(string))
-elif len(string)==8:
-      for i,j in enumerate([1,3,5]):
-             string.insert(i+j, ",")
-      print(''.join(string))
-elif len(string)==9:
-      for i,j in enumerate([2,4,6]):
-             string.insert(i+j, ",")
-      print(''.join(string))
-elif len(string)==10:
-      for i,j in enumerate([1,3,5,7]):
-             string.insert(i+j, ",")
-      print(''.join(string))
-elif len(string)==11:
-      for i,j in enumerate([2,4,6,8]):
+elif (len(string)%2)==0:
+      for b in range(1,len(string)-2,2):
+             numerical_range.append(b)
+      for i,j in enumerate(numerical_range):
              string.insert(i+j, ",")
       print(''.join(string))
 else:
-      print('Range crossed')
+      print('sorry')
